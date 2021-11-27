f944be243e6bd6316c4c9a98207e314cc190aa19
Yoan Mollard <ymollard@users.noreply.github.com>
2021-11-27 16:47:51 +0100

    setup.py update

--- solutions/bank_accounts_simulator/setup.py
+++ solutions/bank_accounts_simulator/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='my_math',
+setup(name='accounts_correction',
       version='1.0',
-      description='Simulator of bank accounts transaction',
+      description='Simulator of bank accounts transactions',
       license='GPL3',
       author='Bob Dupont',
-      author_email='example@example.com',
+      author_email='bob@example.com',
       url='https://advanced.python.training.aubrune.eu',
       packages=find_packages(),
       install_requires=['numpy', 'matplotlib'] # Dependencies to install
